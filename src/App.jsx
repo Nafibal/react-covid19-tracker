8c15381a684233ecdcea5490bd0765b6a75e5ea6
nafibal <baliya363@gmail.com>
2021-04-09 14:57:40 +0700

    Cards component Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import styles from "./App.module.css";
 import { Cards, Chart, CountryPicker } from "./components";
+import { fetchData } from "./api";
 
 const App = () => {
+  const [data, setData] = useState({});
+
+  useEffect(async () => {
+    const data = await fetchData();
+    setData(data);
+  }, []);
+
   return (
     <div className={styles.container}>
-      <Cards />
+      <Cards data={data} />
       <CountryPicker />
       <Chart />
     </div>
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { Card, Typography, Grid, CardContent } from "@material-ui/core";
+
+import styles from "./Cards.module.css";
 
 const Cards = () => {
   return (
-    <div>
-      <h2>Cards</h2>
+    <div className={styles.container}>
+      <Grid container spacing={3} justify="center">
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Infected
+            </Typography>
+            <Typography variant="h5">Data</Typography>
+            <Typography color="textSecondary">Date</Typography>
+            <Typography variant="body2">
+              Number of active cases of COVID-19
+            </Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
     </div>
   );
 };
